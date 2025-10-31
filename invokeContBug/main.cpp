7d14c1ab63b8ae65f4d565eae5e79017ab4e45bd
Nerum <nerumlp@gmail.com>
2025-10-31 15:10:42 +0100

    add invokeContBug sample

--- invokeContBug/main.cpp
+++ invokeContBug/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
 bool feature = true;
-
-void test(int a) {
-    int t = 42;
-    a += t;
-}
+class Test {
+public:
+    Test() = default;
+    void test(int a) {
+        int t = 42;
+        a += t;
+    }
+};
 
 int main() {
-    int a = 42;
-    test(a);
+    int a = 1;
+    Test t{};
+    t.test(a);
     if (feature) {
         a =2;
     }
